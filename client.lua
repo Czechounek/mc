8bcd2ba9fd7ce9a859a645ad2e2547182b7ebcbd
Czechounek <77670717+Czechounek@users.noreply.github.com>
2026-05-14 20:46:36 +0200

    Update unit configuration and scan commands
    Updated commands to use the new storage controller ID and improved print statements.

--- client.lua
+++ client.lua
@@ -1,17 +1,17 @@
 local SERVER_ID = 5
 rednet.open("top")
 
-print("Sending unit configuration to server...")
+print("Sending updated unit configuration to server...")
 
--- Command 1: Create the storage unit
-local setCommand = [[unit.set("storage", {"functionalstorage:storage_controller_0", "jumbofurnace:jumbo_furnace_exterior_0", "metalbarrels:crystal_1"})]]
+-- Updated Command 1: Now using "storage_controller_1"
+local setCommand = [[unit.set("storage", {"functionalstorage:storage_controller_1", "jumbofurnace:jumbo_furnace_exterior_0", "metalbarrels:crystal_1"})]]
 rednet.send(SERVER_ID, {setCommand, 1}, "stockpile")
 
-sleep(1) -- Wait 1 second to let the server process
+sleep(1)
 
--- Command 2: Scan the inventories
+-- Updated Command 2: Scan the inventories again
 print("Sending scan command...")
-local scanCommand = [[scan({"functionalstorage:storage_controller_0", "jumbofurnace:jumbo_furnace_exterior_0", "metalbarrels:crystal_1"})]]
+local scanCommand = [[scan({"functionalstorage:storage_controller_1", "jumbofurnace:jumbo_furnace_exterior_0", "metalbarrels:crystal_1"})]]
 rednet.send(SERVER_ID, {scanCommand, 2}, "stockpile")
 
-print("Done! You can now run your monitor program.")
+print("Done! The server now knows the new ID.")
